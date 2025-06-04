9125914e549a67aac8574ee19a3d85bc01a8e35b
PhysicistsOfDoom <corbin.beus@gmail.com>
2025-06-04 12:01:24 -0600

    Created Read, Write to Store & Output last 2 digits of +10 and +11

--- cpu.cs
+++ cpu.cs
@@ -1,13 +1,42 @@
 
 public class CPU 
 {
-    public void Read() 
-    {
+    private MainMemory memory;
+    private int Counter;
 
+    public CPU(MainMemory mem)
+    {
+        memory = mem;
+        Counter = 0;
     }
-    public void Write() 
+    //Ask for Input, Store in the memory location of the last 2 digits following the "+10xx" 
+    public void Read(int address)
     {
+        //Get Input
+        Console.Write($"Enter a value for memory location {address:D2}: ");
+        int input;
 
+        //Repeatedly ask if input is invalid
+        while (!int.TryParse(Console.ReadLine(), out input))
+        {
+            Console.Write("Enter a VALID Memory Integer: ");
+        }
+
+        //Store Input (input) in memory (address)
+        memory.SetWord(address, input);
+
+        //Increment
+        Counter++;
+    }
+    //Output value of the address after "+11xx"
+    public void Write(int address)
+    {
+        //Get Value at (address) & Output
+        int value = memory.GetWord(address);
+        Console.WriteLine($"Output from memory {address:D2}: {value}");
+
+        //Increment
+        Counter++;
     }
     public void Load() 
     {
