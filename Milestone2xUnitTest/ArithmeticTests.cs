b347193bac1705c913b573807aab51f69edf6cb8
Kylobe <traedonharris29@gmail.com>
2025-06-05 18:08:55 -0600

    Created More test cases

---
+++ Milestone2xUnitTest/ArithmeticTests.cs
@@ -0,0 +1,112 @@
+using Xunit;
+using namespace Milestone2;
+
+public class ArithmeticTests
+{
+    [Fact]
+    public void Add_ChangesAccumulatorCorrectly()
+    {
+        TraversableRegister[] mainMemory = new TraversableRegister[100];
+        Register accumulator = new Register("0000");
+        for (int i = 0; i < mainMemory.Length; i++)
+        {
+            mainMemory[i] = new TraversableRegister("0000");
+        }
+        CPU cpu = new CPU(mainMemory, accumulator);
+        mainMemory[0].Register.RegVal = 1;
+        accumulator.RegVal = 1;
+        cpu.Add(0);
+        Assert.Equal(2, accumulator.RegVal);
+    }
+    [Fact]
+    public void Add_OverFlowsCorrectly()
+    {
+        TraversableRegister[] mainMemory = new TraversableRegister[100];
+        Register accumulator = new Register("0000");
+        for (int i = 0; i < mainMemory.Length; i++)
+        {
+            mainMemory[i] = new TraversableRegister("0000");
+        }
+        CPU cpu = new CPU(mainMemory, accumulator);
+        mainMemory[0].Register.RegVal = 5000;
+        accumulator.RegVal = 5000;
+        cpu.Add(0);
+        Assert.Equal(1000, accumulator.RegVal);
+    }
+    [Fact]
+    public void Subtract_ChangesAccumulatorCorrectly()
+    {
+        TraversableRegister[] mainMemory = new TraversableRegister[100];
+        Register accumulator = new Register("0000");
+        for (int i = 0; i < mainMemory.Length; i++)
+        {
+            mainMemory[i] = new TraversableRegister("0000");
+        }
+        CPU cpu = new CPU(mainMemory, accumulator);
+        mainMemory[0].Register.RegVal = 1;
+        accumulator.RegVal = 10;
+        cpu.Subtract(0);
+        Assert.Equal(9, accumulator.RegVal);
+    }
+    [Fact]
+    public void Subtract_OverFlowsCorrectly()
+    {
+        TraversableRegister[] mainMemory = new TraversableRegister[100];
+        Register accumulator = new Register("0000");
+        for (int i = 0; i < mainMemory.Length; i++)
+        {
+            mainMemory[i] = new TraversableRegister("0000");
+        }
+        CPU cpu = new CPU(mainMemory, accumulator);
+        mainMemory[0].Register.RegVal = -5000;
+        accumulator.RegVal = 5000;
+        cpu.Subtract(0);
+        Assert.Equal(1000, accumulator.RegVal);
+    }
+    [Fact]
+    public void Multiply_ChangesAccumulatorCorrectly()
+    {
+        TraversableRegister[] mainMemory = new TraversableRegister[100];
+        Register accumulator = new Register("0000");
+        for (int i = 0; i < mainMemory.Length; i++)
+        {
+            mainMemory[i] = new TraversableRegister("0000");
+        }
+        CPU cpu = new CPU(mainMemory, accumulator);
+        mainMemory[0].Register.RegVal = 2;
+        accumulator.RegVal = 10;
+        cpu.Multiply(0);
+        Assert.Equal(20, accumulator.RegVal);
+    }
+    [Fact]
+    public void Multiply_OverFlowsCorrectly()
+    {
+        TraversableRegister[] mainMemory = new TraversableRegister[100];
+        Register accumulator = new Register("0000");
+        for (int i = 0; i < mainMemory.Length; i++)
+        {
+            mainMemory[i] = new TraversableRegister("0000");
+        }
+        CPU cpu = new CPU(mainMemory, accumulator);
+        mainMemory[0].Register.RegVal = 2;
+        accumulator.RegVal = 5000;
+        cpu.Multiply(0);
+        Assert.Equal(1000, accumulator.RegVal);
+    }
+    [Fact]
+    public void Divide_ChangesAccumulatorCorrectly()
+    {
+        TraversableRegister[] mainMemory = new TraversableRegister[100];
+        Register accumulator = new Register("0000");
+        for (int i = 0; i < mainMemory.Length; i++)
+        {
+            mainMemory[i] = new TraversableRegister("0000");
+        }
+        CPU cpu = new CPU(mainMemory, accumulator);
+        mainMemory[0].Register.RegVal = 2;
+        accumulator.RegVal = 10;
+        cpu.Divide(0);
+        Assert.Equal(5, accumulator.RegVal);
+    }
+}
+
