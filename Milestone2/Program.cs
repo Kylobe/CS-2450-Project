74a539f1de9b57eeed943c1cb0d6661485119bed
daniel <danlewurling@gmail.com>
2025-06-06 17:14:36 -0600

    Add revisions to xunit to work with current read and write operations, and change program start up

--- Milestone2/Program.cs
+++ Milestone2/Program.cs
@@ -3,12 +3,28 @@ namespace Milestone2;
 
 public class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         UVSim uvSim = new UVSim();
-        uvSim.LoadArray(["1008", "4300"]); // load file name
+        string filePath = string.Empty;
+        
+        filePath = args.Length > 0 
+            ? args[0] 
+            : GetFilePath();
+        
+        uvSim.LoadFile(filePath); 
         uvSim.Run();
         Console.WriteLine("End of Program\nPress any key to exit...");
         Console.ReadKey();
+    }
+    static string GetFilePath()
+    {
+        string filePath = Console.ReadLine() ?? string.Empty;
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"File {filePath} does not exist or cannot be accessed!");
+            filePath = GetFilePath();
+        }
+        return filePath;
     }
 }
